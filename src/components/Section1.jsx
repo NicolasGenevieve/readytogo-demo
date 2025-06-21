a99e60d5ca578472d5d4e2c3ec4957a003718bd5
Nicolas Genevieve <contact@nicolasgenevieve.com>
2025-06-21 15:31:14 +0200

    change components switcher in only one button

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -11,14 +11,9 @@ const Section1 = ({
 }) => {
   return (
     <section className="section1">
-      <Switcher
-        isOff1={isOff1}
-        setIsOff1={setIsOff1}
-        isOff2={isOff2}
-        setIsOff2={setIsOff2}
-        isOff3={isOff3}
-        setIsOff3={setIsOff3}
-      />
+      <Switcher state={isOff1} setState={setIsOff1} />
+      <Switcher state={isOff2} setState={setIsOff2} />
+      <Switcher state={isOff3} setState={setIsOff3} />
     </section>
   );
 };
--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,66 +1,23 @@
 import "./Switcher.css";
 import "./Result.css";
 
-const Switcher = ({
-  isOff1,
-  setIsOff1,
-  isOff2,
-  setIsOff2,
-  isOff3,
-  setIsOff3,
-}) => {
+const Switcher = ({ state, setState }) => {
   return (
     <>
       <div className="switcher">
         <button
           onClick={() => {
-            setIsOff1(true);
+            setState(true);
           }}
-          className={isOff1 === true ? "push" : ""}
+          className={state ? "push" : ""}
         >
           On
         </button>
         <button
           onClick={() => {
-            setIsOff1(false);
+            setState(false);
           }}
-          className={isOff1 === false ? "push" : ""}
-        >
-          Off
-        </button>
-      </div>
-      <div className="switcher">
-        <button
-          onClick={() => {
-            setIsOff2(true);
-          }}
-          className={isOff2 === true ? "push" : ""}
-        >
-          On
-        </button>
-        <button
-          onClick={() => {
-            setIsOff2(false);
-          }}
-          className={isOff2 === false ? "push" : ""}
-        >
-          Off
-        </button>
-      </div>
-      <div className="switcher">
-        <button
-          onClick={() => {
-            setIsOff3(true);
-          }}
-          className={isOff3 === true ? "push" : ""}
-        >
-          On
-        </button>
-        <button
-          onClick={() => {
-            setIsOff3(false);
-          }}
-          className={isOff3 === false ? "push" : ""}
+          className={!state ? "push" : ""}
         >
           Off
         </button>
